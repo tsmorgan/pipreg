848218dde0c9b24441970a6d9f55d6d3afc33fc1
Tom Morgan <tsmorgan@gmail.com>
2020-08-17 16:36:23 +0100

    update the port thing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+const PORT = process.env.PORT || 8125;
 http.createServer(function (request, response) {
     console.log('request starting...');
 
@@ -52,5 +53,5 @@ http.createServer(function (request, response) {
         }
     });
 
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+}).listen(PORT);
+console.log('Server running at http://127.0.0.1:'+PORT+'/');
